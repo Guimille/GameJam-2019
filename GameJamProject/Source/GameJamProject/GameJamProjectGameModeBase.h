ebf4cf2a5d55cfbb6bd165757d9c4418fdf2f0f1
lplancher <louis.plancher@epitech.eu>
2019-01-25 22:16:28 +0100

    sources

---
+++ GameJamProject/Source/GameJamProject/GameJamProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameJamProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAMPROJECT_API AGameJamProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
